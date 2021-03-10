df22b3f34cd4b121c241a13eb8d2ab8a5edc0138
mmergal92 <mmergal92@gmail.com>
2021-03-10 18:09:49 -0500

    verbal questions completed

---
+++ solutions.js
@@ -0,0 +1,3 @@
+// Section 1
+//1. Difference between a parameter and argument is that the parameters are the variables or names that you put into function. Then when you're calling the function you use arguments that are meant to plug into where the variables were and carry out that function.
+//2. Difference between a return and a console.log is that a return allows you to store the function and return a value or the answer to the function later on if it's called. A console.log can tell you if the function will work or not, but it's not actually storing anything or returning any value when the function is called.
